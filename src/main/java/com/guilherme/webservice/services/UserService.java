f19aecba84fe98d2c4cd9604300dab82ab6afb8d
Guilherme Cardozo <guilhermecardozo91@gmail.com>
2023-11-18 16:01:06 -0300

    service layer, component registration

---
+++ src/main/java/com/guilherme/webservice/services/UserService.java
@@ -0,0 +1,26 @@
+package com.guilherme.webservice.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.guilherme.webservice.entities.User;
+import com.guilherme.webservice.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll() {
+		return userRepository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> user = userRepository.findById(id);
+		return user.get();
+	}
+}
