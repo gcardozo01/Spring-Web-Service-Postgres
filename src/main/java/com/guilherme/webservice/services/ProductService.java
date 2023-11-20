1ad748cacf9cc6875b621650bc3cbd0ec21dd9c8
Guilherme Cardozo <guilhermecardozo91@gmail.com>
2023-11-20 15:18:23 -0300

    Product

---
+++ src/main/java/com/guilherme/webservice/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.guilherme.webservice.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.guilherme.webservice.entities.Product;
+import com.guilherme.webservice.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	public List<Product> findAll() {
+		return productRepository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> product = productRepository.findById(id);
+		return product.get();
+	}
+}
