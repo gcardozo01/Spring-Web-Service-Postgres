e821ce8366e75b1805a8dc0134af4f0e2d7d61a5
Guilherme Cardozo <guilhermecardozo91@gmail.com>
2023-11-20 14:57:19 -0300

    category

--- src/main/java/com/guilherme/webservice/config/TestConfig.java
+++ src/main/java/com/guilherme/webservice/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.guilherme.webservice.entities.Category;
 import com.guilherme.webservice.entities.Order;
 import com.guilherme.webservice.entities.User;
 import com.guilherme.webservice.entities.enums.OrderStatus;
+import com.guilherme.webservice.repositories.CategoryRepository;
 import com.guilherme.webservice.repositories.OrderRepository;
 import com.guilherme.webservice.repositories.UserRepository;
 
@@ -22,17 +24,26 @@ public class TestConfig implements CommandLineRunner {
 	private UserRepository userRepository;
 	
 	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	@Autowired
 	private OrderRepository orderRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers"); 
+		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
 		Order o1 = new Order(null, Instant.parse("2023-06-20T19:53:07Z"), OrderStatus.WAITING_PAYMENT, u1);
 		Order o2 = new Order(null, Instant.parse("2023-07-21T03:42:10Z"), OrderStatus.PAID, u2);
 		Order o3 = new Order(null, Instant.parse("2023-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
-		
+
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 	}
---
+++ src/main/java/com/guilherme/webservice/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.guilherme.webservice.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.guilherme.webservice.entities.Category;
+import com.guilherme.webservice.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	public List<Category> findAll() {
+		return categoryRepository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> category = categoryRepository.findById(id);
+		return category.get();
+	}
+}
