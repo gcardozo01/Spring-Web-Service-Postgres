6e1179dcf64488308daf16d92ddfc4aae9b5bf45
Guilherme Cardozo <guilhermecardozo91@gmail.com>
2023-11-22 17:31:55 -0300

    Subtotal & Total methods

--- src/main/java/com/guilherme/webservice/entities/Payment.java
+++ src/main/java/com/guilherme/webservice/entities/Payment.java
@@ -3,6 +3,8 @@ package com.guilherme.webservice.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,6 +24,7 @@ public class Payment implements Serializable {
 	private Long id;
 	private Instant momment;
 	
+	@JsonIgnore
 	@OneToOne
 	@MapsId
 	private Order order;
