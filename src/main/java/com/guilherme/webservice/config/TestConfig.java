6395fe055d42b1c98ddb851b75b1fc1c48771420
Guilherme Cardozo <guilhermecardozo91@gmail.com>
2023-11-22 17:22:05 -0300

    Payment, one-to-one association

--- src/main/java/com/guilherme/webservice/config/TestConfig.java
+++ src/main/java/com/guilherme/webservice/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.guilherme.webservice.entities.Category;
 import com.guilherme.webservice.entities.Order;
 import com.guilherme.webservice.entities.OrderItem;
+import com.guilherme.webservice.entities.Payment;
 import com.guilherme.webservice.entities.Product;
 import com.guilherme.webservice.entities.User;
 import com.guilherme.webservice.entities.enums.OrderStatus;
@@ -78,5 +79,9 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2023-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 }
