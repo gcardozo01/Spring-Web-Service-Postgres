81f5f0ef6c55e4be86cc3020d31a5334d960286d
Guilherme Cardozo <guilhermecardozo91@gmail.com>
2023-11-24 20:08:48 -0300

    Exception handling - update

--- src/main/java/com/guilherme/webservice/services/UserService.java
+++ src/main/java/com/guilherme/webservice/services/UserService.java
@@ -13,6 +13,8 @@ import com.guilherme.webservice.repositories.UserRepository;
 import com.guilherme.webservice.services.exceptions.DatabaseException;
 import com.guilherme.webservice.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -43,9 +45,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User user = userRepository.getReferenceById(id);
-		updateData(user, obj);
-		return userRepository.save(user);
+		try {
+			User user = userRepository.getReferenceById(id);
+			updateData(user, obj);
+			return userRepository.save(user);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User user, User obj) {
